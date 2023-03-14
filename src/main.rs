018a5e6ef4ff9445151f7c8d57bc718c93e8fb9e
Max Crone <code@maxcrone.org>
2023-03-14 12:59:25 +0100

    chore: cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -21,10 +21,13 @@ struct Event {
     end: String,
 }
 
-impl From::<&Value> for Event {
+impl From<&Value> for Event {
     fn from(value: &Value) -> Self {
         Event {
-            person: value["event_who_profile_call_name"].as_str().unwrap().to_owned(),
+            person: value["event_who_profile_call_name"]
+                .as_str()
+                .unwrap()
+                .to_owned(),
             event_type: value["event_type"].as_str().unwrap().to_owned(),
             date: value["event_date"].as_str().unwrap().to_owned(),
             start_to_end: value["event_start_end_time"].as_str().unwrap().to_owned(),
@@ -48,8 +51,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     loop {
         let url = format!(
             "https://denieuweanita.l1nda.nl/week/{}/{}?xhr=true",
-            year,
-            week
+            year, week
         )
         .parse::<Url>()
         .unwrap();
@@ -64,34 +66,53 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         println!("Got data from {} to {}", v["start_date"], v["end_date"]);
 
-        events.extend(v["scheduled_events"]
-            .as_array()
-            .unwrap()
-            .get(0)
-            .unwrap()
-            .get("layer_days")
-            .unwrap()
-            .as_array()
-            .unwrap()
-            .iter()
-            .filter(|day| day.get("day_key").unwrap().as_str().unwrap().starts_with(&cli.month))
-            .flat_map(|day| day.get("day_events").unwrap().as_array().unwrap().iter())
-            .filter(|&event| {
-                event
-                    .get("event_who_profile_call_name")
-                    .unwrap()
-                    .as_str()
-                    .unwrap()
-                    == cli.name
-            })
-            .map(Event::from)
+        events.extend(
+            v["scheduled_events"]
+                .as_array()
+                .unwrap()
+                .get(0)
+                .unwrap()
+                .get("layer_days")
+                .unwrap()
+                .as_array()
+                .unwrap()
+                .iter()
+                .filter(|day| {
+                    day.get("day_key")
+                        .unwrap()
+                        .as_str()
+                        .unwrap()
+                        .starts_with(&cli.month)
+                })
+                .flat_map(|day| day.get("day_events").unwrap().as_array().unwrap().iter())
+                .filter(|&event| {
+                    event
+                        .get("event_who_profile_call_name")
+                        .unwrap()
+                        .as_str()
+                        .unwrap()
+                        == cli.name
+                })
+                .map(Event::from),
         );
 
         // Break out if we are currently starting in a week that is past the
         // month we were processing.
-        let start_month = v["start_date"].as_str().unwrap().split('-').take(2).last().unwrap();
-        let end_month = v["end_date"].as_str().unwrap().split('-').take(2).last().unwrap();
-        if start_month > month  || end_month > month {
+        let start_month = v["start_date"]
+            .as_str()
+            .unwrap()
+            .split('-')
+            .take(2)
+            .last()
+            .unwrap();
+        let end_month = v["end_date"]
+            .as_str()
+            .unwrap()
+            .split('-')
+            .take(2)
+            .last()
+            .unwrap();
+        if start_month > month || end_month > month {
             break;
         }
 
@@ -99,12 +120,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     for e in &events {
-        println!(
-            "{} works at {} from {}",
-            e.person,
-            e.date,
-            e.start_to_end
-        );
+        println!("{} works at {} from {}", e.person, e.date, e.start_to_end);
     }
 
     if events.is_empty() {
@@ -121,29 +137,13 @@ fn invoice(events: &[Event]) {
     println!(
         "Factuur De Nieuwe Anita ({} t/m {})",
         events[0].date,
-        events
-            .last()
-            .unwrap()
-            .date
+        events.last().unwrap().date
     );
     println!("service:");
     for e in events {
-        let desc = format!(
-            "{} {} ({})",
-            e.event_type,
-            e.date,
-            e.start_to_end
-        );
-        let start = DateTime::parse_from_rfc3339(&format!(
-            "{}+01:00",
-            e.start
-        ))
-        .unwrap();
-        let end = DateTime::parse_from_rfc3339(&format!(
-            "{}+01:00",
-            e.end
-        ))
-        .unwrap();
+        let desc = format!("{} {} ({})", e.event_type, e.date, e.start_to_end);
+        let start = DateTime::parse_from_rfc3339(&format!("{}+01:00", e.start)).unwrap();
+        let end = DateTime::parse_from_rfc3339(&format!("{}+01:00", e.end)).unwrap();
         let hours = (end - start).num_minutes() as f64 / 60.0;
         let tarief = 18.0;
         let total = hours * tarief;
