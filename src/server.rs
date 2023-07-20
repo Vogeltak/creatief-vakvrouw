b13dcd0b24e379c8c261261b008697caca0ca033
Max Crone <code@maxcrone.org>
2023-07-20 23:58:44 +0200

    Add BTW aangifte overview page

--- src/server.rs
+++ src/server.rs
@@ -46,6 +46,12 @@ struct HistoryTemplate {
     grouped_invoices: Vec<(YearMonth, Vec<Factuur>)>,
 }
 
+#[derive(Template)]
+#[template(path = "btw.html")]
+struct BtwTemplate {
+    quarters: Vec<(Quarter, Btw)>,
+}
+
 mod filters {
     use chrono::{TimeZone, Utc};
 
@@ -84,6 +90,7 @@ pub async fn run() -> Result<()> {
         .route("/factuur", get(factuur_get))
         .route("/factuur", post(factuur_post))
         .route("/alle", get(history_get))
+        .route("/btw", get(btw_get))
         .with_state(state);
 
     let server = Server::bind(&"0.0.0.0:1728".parse()?).serve(router.into_make_service());
@@ -301,4 +308,84 @@ async fn history_get(State(state): State<AppState>) -> HistoryTemplate {
     grouped_invoices.reverse();
 
     HistoryTemplate { grouped_invoices }
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+struct Quarter {
+    year: i32,
+    quarter: u32,
+}
+
+impl std::fmt::Display for Quarter {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let quarters = HashMap::from([
+            (1, "jan–maa"),
+            (2, "apr–jun"),
+            (3, "jul–sep"),
+            (4, "okt–dec"),
+        ]);
+
+        let display_quarter = match quarters.get(&self.quarter) {
+            Some(q) => q,
+            None => "ooit",
+        };
+
+        write!(f, "{}Q{} ({})", self.year, self.quarter, display_quarter)
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Btw {
+    omzet: f64,
+    btw: f64,
+    invoices: Vec<Factuur>,
+}
+
+async fn btw_get(State(state): State<AppState>) -> BtwTemplate {
+    let mut conn = state.db.acquire().await.unwrap();
+    let mut invoices = match get_invoices(&mut conn).await {
+        Ok(invoices) => invoices,
+        Err(err) => {
+            println!("Failed to fetch invoices from DB: {:?}", err);
+            vec![]
+        }
+    };
+
+    invoices.sort_by_key(|i| i.nummer);
+    invoices.reverse();
+
+    let mut grouped_invoices = HashMap::new();
+
+    // Group by quarter
+    for i in invoices {
+        let q = Quarter {
+            year: i.date.year(),
+            // Default division behavior on unsigned integers is to floor
+            quarter: i.date.month() / 4 + 1,
+        };
+        grouped_invoices.entry(q).or_insert(vec![]).push(i);
+    }
+
+    let mut grouped_invoices: Vec<(Quarter, Btw)> = grouped_invoices
+        .into_iter()
+        .map(|(q, invoices)| {
+            let omzet = invoices.iter().map(|i| i.subtotal).sum();
+            let btw = invoices.iter().map(|i| i.btw).sum();
+            (
+                q,
+                Btw {
+                    omzet,
+                    btw,
+                    invoices,
+                },
+            )
+        })
+        .collect();
+
+    grouped_invoices.sort_by(|(ym1, _), (ym2, _)| ym1.cmp(ym2));
+    grouped_invoices.reverse();
+
+    BtwTemplate {
+        quarters: grouped_invoices,
+    }
 }
